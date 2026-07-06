9b5bf0c2eb485aef7138fdf549aaa57e895014b0
Daniel Mercado <60846433+Damet24@users.noreply.github.com>
2026-07-06 15:45:41 -0500

    feat(http server): bootstrap

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Server } from './server';
+import { env } from './config/env.config';
+import logger from './config/logger';
+
+const server = new Server(String(env.port));
+
+server.listen().catch((err) => logger.error('Server failed to start', err));
